2488a895395e8c441dca24102d467a0dac76fdbb
quyet <quyet@whl.travel>
2020-03-28 19:05:59 +0700

    db

---
+++ db.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog Professional v13.1.1 (64 bit)
+MySQL - 10.4.11-MariaDB : Database - bill_create
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`bill_create` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `bill_create`;
+
+/*Table structure for table `bill` */
+
+DROP TABLE IF EXISTS `bill`;
+
+CREATE TABLE `bill` (
+  `id` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `customer_name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `all_data` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `total` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `date_created` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `bill` */
+
+insert  into `bill`(`id`,`customer_name`,`all_data`,`total`,`date_created`) values 
+('1585396060','ghghghgh','ghghghg','100','2020-03-28 12:47:40');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
